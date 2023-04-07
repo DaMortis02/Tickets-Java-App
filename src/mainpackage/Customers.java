36d26b3733dc6c9dc3700ea8bfc519e8b05af055
Mortis <77734605+DaMortis02@users.noreply.github.com>
2023-04-07 14:51:37 +0300

    Update Customers.java

--- src/mainpackage/Customers.java
+++ src/mainpackage/Customers.java
@@ -3,11 +3,13 @@ package mainpackage;
 public class Customers extends Users {
     // Attributes
     private String reservationId;
+    private int age;
 
     // Constructor
-    public Customers(String name, String username, String password, String email, String reservationId) {
+    public Customers(String name, String username, String password, String email, String reservationId, int age) {
         super(name, username, password, email);
         this.reservationId = reservationId;
+        this.age = age;
 
     // Getters and Setters
     public String getReservationId() {
@@ -16,6 +18,14 @@ public class Customers extends Users {
 
     public void setReservationId(String reservationId) {
         this.reservationId = reservationId;
+    }
+        
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 
     // Methods
